02c7d91cb9eaaab38117179628720569fcf9c880
Xtarii <carlalvinelias@gmail.com>
2024-12-13 13:32:50 +0100

    ENV fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: process.env.NEXT_PUBLIC_NODE_ENV === "production" ? "/Math-for-Fun---Better-math-website" : "",
+  // output: "export",
+  reactStrictMode: true
 };
 
 export default nextConfig;
