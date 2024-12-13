2ee11305452023aca224ce0937abb9e428da1ce8
Xtarii <carlalvinelias@gmail.com>
2024-12-13 14:46:43 +0100

    Default config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: process.env.NEXT_PUBLIC_NODE_ENV === "production" ? "/" : "",
 };
 
 export default nextConfig;
