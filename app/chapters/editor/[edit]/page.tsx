fe5884806ee8e0fc8d747815654db8b6b2ca4d43
Xtarii <carlalvinelias@gmail.com>
2024-12-22 20:16:20 +0100

    new input types

--- app/chapters/editor/[edit]/page.tsx
+++ app/chapters/editor/[edit]/page.tsx
@@ -6,6 +6,8 @@ import { getUser } from "@/utils/supabase/account/auth";
 import { getQuestionByID, insertQuestion, updateQuestion } from "@/utils/supabase/database/database";
 import { useRouter } from "next/navigation";
 import { FormEvent, ReactElement, useEffect, useState } from "react";
+import ListInput from "@/components/ui/forms/inputs/listInput";
+import ImageInput from "@/components/ui/forms/inputs/imageInput";
 
 export default function Edit({params} : { params: Promise<{ edit: "new" | string }> }) : ReactElement {
     const router = useRouter();
@@ -132,21 +134,11 @@ export default function Edit({params} : { params: Promise<{ edit: "new" | string
                 <div className="w-fit h-screen mx-2">
                     <h1 className="text-xl text-center mx-auto">Optional</h1>
 
-                    <p>Under development - Do not use this as this will have no effect on the content added</p>
 
-
-
-                    <div className="flex items-center justify-center w-full">
-                        <label htmlFor="image" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                            <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                                </svg>
-                                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                                <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                            </div>
-                            <input className="hidden" name="image" id="image" type="file" placeholder="Image" value={image} onChange={e => setImage(e.target.value)} />
-                        </label>
+                    <ImageInput className="w-[32rem]" />
+                    <div className="mt-10">
+                        <label htmlFor="flags" className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Flags</label>
+                        <ListInput name="flags" id="flags" className="overflow-hidden max-w-[32rem]" default={flags} onChange={setFlags} placeholder="Flags" />
                     </div>
                 </div>
 
