23fd0502ff61c73218b2073bcd64e2a6276ea879
Xtarii <carlalvinelias@gmail.com>
2024-12-28 15:16:00 +0100

    better message box
    Fixed the props and children params for the message box

--- components/ui/forms/messages/messageBox.tsx
+++ components/ui/forms/messages/messageBox.tsx
@@ -10,23 +10,23 @@ import { ReactElement } from "react";
  * the page unusable by the user - only the
  * message box can be interacted with.
  */
-export default function MessageBox(props: { className?: string, message?: string, title?: string }, children?: ReactElement) : ReactElement {
+export default function MessageBox({ children, className, title, message }: { children?: ReactElement, className?: string, title?: string, message?: string }) : ReactElement {
     return(<div className="relative">
         <div className="fixed inset-0 flex items-center justify-center z-50">
             <div className="absolute inset-0 bg-gray-900 dark:bg-slate-900 opacity-50" />
 
             <div className="relative z-10">
-                <div className={props.className}>
+                <div className={className}>
                     {/* Header */}
                     <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                         <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                            {props.title}
+                            {title}
                         </h3>
                     </div>
                     {/* Content */}
                     <div className="p-4 md:p-5 space-y-4">
                         <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            {props.message}
+                            {message}
                         </p>
                     </div>
 
