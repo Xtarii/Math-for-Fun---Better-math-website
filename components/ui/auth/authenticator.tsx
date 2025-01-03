c9d645b75fb38a8456376df3f5301dabcc9a633e
Xtarii <carlalvinelias@gmail.com>
2025-01-03 16:32:53 +0100

    Added a auth element
    - No functions so far

---
+++ components/ui/auth/authenticator.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react";
+
+/**
+ * Authenticator Element
+ *
+ * Supabase react authentication
+ * element. Holds functions to
+ * login and create accounts.
+ */
+export default function Authenticator() : ReactElement {
+    return(<div>
+    </div>);
+}
