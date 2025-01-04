1ec5d11dae692553292b95144fdd5f73d8b881ae
Xtarii <carlalvinelias@gmail.com>
2025-01-04 16:03:00 +0100

    Account page style

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import LoadingWheel from "@/components/loading/wheel";
+import MessageBox from "@/components/ui/forms/messages/messageBox";
 import { getUser, signOut } from "@/utils/supabase/account/auth";
+import { Button } from "@mui/material";
 import { User } from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
 import { ReactElement, useEffect, useState } from "react";
@@ -9,6 +11,7 @@ export default function Account() : ReactElement {
     const router = useRouter();
     const [ load, setLoad ] = useState<boolean>(true);
     const [ user, setUser ] = useState<User>();
+    const [ error, setError ] = useState<boolean>(false);
 
 
     // Account Data
@@ -32,41 +35,59 @@ export default function Account() : ReactElement {
     // Page Content
     return(<div>
         {load && <LoadingWheel />}
+        {error && <MessageBox
+            className="relative bg-white rounded-lg shadow dark:bg-gray-700"
+            title="Invalid Request"
+            message='Could not delete your account, an error occurred! Try again later.'>
+                <Button variant="outlined" color="error" onClick={(e) => {
+                    e.preventDefault();
+                    setError(false);
+                    router.refresh();
+                }}>Close</Button>
+        </MessageBox>}
 
 
         {/* Page content */}
-        <div className="w-screen h-screen flex">
+        <div className="w-screen h-screen flex flex-wrap">
             <h1>Welcome user : {user?.email}</h1>
 
-            <button onClick={async () => {
-                setLoad(true);
-                await signOut();
-
-                setLoad(false);
-            }}>Sign Out</button>
 
 
-            <button onClick={async (e) => {
-                e.preventDefault();
-                if(!user) return;
-                setLoad(true);
+            <div className="w-full mx-4 flex">
+                <div className="mr-2">
+                    <Button variant="outlined" color="warning" onClick={async () => {
+                        setLoad(true);
+                        await signOut();
+                        router.push("/account/login");
+                        setLoad(false);
+                    }}>Sign Out</Button>
+                </div>
+                <div className="ml-2">
+                    <Button variant="contained" color="error" onClick={async (e) => {
+                        e.preventDefault();
+                        if(!user) {
+                            setError(true);
+                            return;
+                        }
+                        setLoad(true);
 
-                // Deletes user
-                const response: { message: "success" | "error" } = await (await fetch('/api/account', {
-                    method: 'DELETE',
-                    headers: {
-                      'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ id: user.id }),
-                })).json();
-                if(response.message === "success") {
-                    await signOut("global");
-                    router.push("/account/login");
-                } else {
-                }
-
-                setLoad(false);
-            }}>Delete Account</button>
+                        // Deletes user
+                        const response: { message: "success" | "error" } = await (await fetch('/api/account', {
+                            method: 'DELETE',
+                            headers: {
+                            'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({ id: user.id }),
+                        })).json();
+                        if(response.message === "success") {
+                            await signOut("global");
+                            router.push("/account/login");
+                        }
+                        setError(true);
+                        setLoad(false);
+                    }}>Delete Account</Button>
+                </div>
+            </div>
         </div>
     </div>);
 }
