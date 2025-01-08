23c1f89f5d4fb25a7a08d140f11b82a1cff63325
Xtarii <carlalvinelias@gmail.com>
2025-01-08 10:00:38 +0100

    Suspense

--- app/account/login/page.tsx
+++ app/account/login/page.tsx
@@ -1,8 +1,11 @@
+import LoadingWheel from "@/components/loading/wheel";
 import Login from "@/components/ui/auth/account/login";
-import { ReactElement } from "react";
+import { ReactElement, Suspense } from "react";
 
 export default function Page() : ReactElement {
     return(<div className="w-screen h-screen flex">
-        <Login className="m-auto w-[32rem] h-[32rem]" />
+        <Suspense fallback={<LoadingWheel />}>
+            <Login className="m-auto w-[32rem] h-[32rem]" />
+        </Suspense>
     </div>);
 }
