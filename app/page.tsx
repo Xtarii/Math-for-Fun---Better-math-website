5d720031b4e8a82872f5e8a647008fc4c52057c7
Xtarii <carlalvinelias@gmail.com>
2025-01-08 10:13:11 +0100

    Temporary help message

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,17 @@ export default function Home() {
 
         {/* Main Content */}
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <a href="/chapters" className="bg-blue-800 hover:bg-blue-900 text-center w-64 h-8 rounded-2xl m-auto">
-            {"-->"} Kapitel Listan
-          </a>
+            <a href="/chapters" className="bg-blue-800 hover:bg-blue-900 text-center w-64 h-8 rounded-2xl m-auto">
+                {"-->"} Kapitel Listan
+            </a>
+
+
+
+            {/* Contact info */}
+            <div>
+                <p>Några problem eller buggar på hemsidan?</p>
+                <p>Ta gärna kontakt med mig, <span className="text-blue-500">alvin.hansen@elev.ga.ntig.se</span></p>
+            </div>
         </main>
     </div>);
 }
