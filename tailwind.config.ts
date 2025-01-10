14500bf7a400a6b6e915a559a3499929f945723a
Xtarii <56205541+Xtarii@users.noreply.github.com>
2025-01-10 15:31:18 +0100

    Update tailwind.config.ts
    material ui for/with tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,14 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  // Material UI
+  corePlugins: {
+    preflight: false,
+  },
+  important: '#__next',
+
+
+  // Tailwind Config
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
