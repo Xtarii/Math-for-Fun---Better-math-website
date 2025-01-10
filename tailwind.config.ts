c7f65dd96b390bab0ba58897239374754892dbda
Xtarii <carlalvinelias@gmail.com>
2025-01-10 14:02:52 +0100

    Changed tailwind config colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,14 +12,6 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
 
-        "slate-variant": "#0F1F25",
-        "grayish": "#2a2e35",
-
-        "blue-var": "#1181c4",
-        "blue-var-hover": "#0d6091",
-        "blu-var2": "#72c2f2",
-        "blue-var3": "#4d64f4",
-
         primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"},
       },
     },
