666e156347a0ea941cff7929931d8c41b73200d1
Xtarii <carlalvinelias@gmail.com>
2025-01-13 10:40:33 +0100

    login verifier function update
    - no children then the function will return a redirect for the hole page.
    
    - children as parameter, then the function will return the children if the user is logged in

--- components/auth/loginVerifier.tsx
+++ components/auth/loginVerifier.tsx
@@ -24,6 +24,48 @@ import Loader from "../ui/load/loader";
  * <LoginVerifier redirect="this" /> // Will be redirected to this page
  * ```
  */
+export default function LoginVerifier({ className, loader, redirect }: { className?: string, loader?: boolean, redirect?: string }) : ReactElement;
+/**
+ * Login Verifier Element
+ *
+ * If the client is not logged in
+ * then this will redirect the client
+ * to the ```login``` page.
+ *
+ * If redirect is not null the login
+ * page will redirect to what page the
+ * redirect is pointing to.
+ * If redirect is ```this``` then it
+ * will be redirected back to the page
+ * that had this element.
+ *
+ * ```tsx
+ * <LoginVerifier redirect="/my/page" /> // Redirects to page
+ *
+ * <LoginVerifier redirect="this" /> // Will be redirected to this page
+ * ```
+ */
+export default function LoginVerifier({ className, loader, redirect }: { className?: string, loader?: boolean, redirect?: "this" }) : ReactElement;
+/**
+ * Login Verifier Element
+ *
+ * If the client is not logged in
+ * then this will redirect the client
+ * to the ```login``` page.
+ *
+ * If redirect is not null the login
+ * page will redirect to what page the
+ * redirect is pointing to.
+ * If redirect is ```this``` then it
+ * will be redirected back to the page
+ * that had this element.
+ *
+ * ```tsx
+ * <LoginVerifier redirect="/my/page" /> // Redirects to page
+ *
+ * <LoginVerifier redirect="this" /> // Will be redirected to this page
+ * ```
+ */
 export default function LoginVerifier({ children, className, loader, redirect }: { children?: ReactNode, className?: string, loader?: boolean, redirect?: string }) : ReactElement;
 /**
  * Login Verifier Element
@@ -49,6 +91,7 @@ export default function LoginVerifier({ children, className, loader, redirect }:
 export default function LoginVerifier({ children, className, loader, redirect }: { children?: ReactNode, className?: string, loader?: boolean, redirect?: "this" | string }) : ReactElement {
     const router = useRouter();
     const [ load, setLoad ] = useState<boolean>(loader || false);
+    const [ isLoggedIn,  setIsLoggedIn ] = useState<boolean>(false);
     const urlRedirect = redirect ? "?redirect=" + (redirect === "this" ? usePathname() : redirect) : "";
 
 
@@ -56,19 +99,23 @@ export default function LoginVerifier({ children, className, loader, redirect }:
     useEffect(() => {
         (async () => {
             const user = await getUser();
-            if(!user) {
+
+            // Handle Login
+            setIsLoggedIn(user !== null);
+            if(!user && !children) {
                 router.push("/account/login" + urlRedirect);
                 return;
             }
             setLoad(false);
         })()
-    }, [])
+    }, [isLoggedIn])
 
 
 
     // Content
-    return(<div className={className}>
+    if(children) return(<div className={className}>
         {load && <Loader />}
-        {!load && children}
+        {(isLoggedIn && !load) && children}
     </div>);
+    return(<div className={className}>{load && <Loader />}</div>);
 }
