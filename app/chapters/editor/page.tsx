e7e8952234bf893777242096e6d1ca8e2c5726e1
Xtarii <carlalvinelias@gmail.com>
2025-01-13 10:40:57 +0100

    better "new button" in editor

--- app/chapters/editor/page.tsx
+++ app/chapters/editor/page.tsx
@@ -1,29 +1,14 @@
 "use client"
-import Loader from "@/components/ui/load/loader";
-import { getUser } from "@/utils/supabase/account/auth";
-import { useRouter } from "next/navigation";
-import { ReactElement, useEffect, useState } from "react";
+import LoginVerifier from "@/components/auth/loginVerifier";
+import { Button } from "@mui/material";
+import { ReactElement } from "react";
 
 export default function Editor() : ReactElement {
-    const router = useRouter();
-    const [ load, setLoad ] = useState<boolean>(true);
-
-
-
-    // Checks account
-    useEffect(() => {
-        (async () => {
-            // Anonyms users can't add content
-            if(!await getUser()) router.push("/account/login");
-            setLoad(false);
-        })()
-    }, [router]);
-
-
-
-    // Content
     return(<div>
-        {load && <Loader />}
-        <button onClick={() => router.push("/chapters/editor/new")}>New</button>
+        <LoginVerifier redirect="this" />
+
+        <div className="flex w-screen h-fit min-h-96">
+            <Button className="m-auto" variant="outlined" href="/chapters/editor/new">Ny Uppgift</Button>
+        </div>
     </div>);
 }
