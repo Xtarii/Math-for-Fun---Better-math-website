7c030ecbeac6fd203c744acb3b0f32c4d85e89a7
Xtarii <carlalvinelias@gmail.com>
2025-01-14 13:58:33 +0100

    removed unused layout

--- app/chapters/[chap]/[question]/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactElement, ReactNode } from "react";
-
-// Page Layouts
-export default function QuestionLayout({ children }: Readonly<{ children: ReactNode }>) : ReactElement {
-    return(<section>{children}</section>);
-}
