badec694ee50a1d21fc247997fa9fe84a1c944ec
Xtarii <carlalvinelias@gmail.com>
2025-01-17 14:24:23 +0100

    chapters question style update

--- app/chapters/page.tsx
+++ app/chapters/page.tsx
@@ -25,7 +25,7 @@ export default function Chapters() : ReactElement {
         {load && <Loader />}
 
         {/* Page Content */}
-        {chapters?.map((value, index) => <div className="w-96 h-14 rounded bg-slate-800 hover:bg-slate-500" key={index}>
+        {chapters?.map((value, index) => <div className="w-full md:w-96 h-14 rounded bg-slate-800 hover:bg-slate-500 border-b-slate-600 border-b-2 md:border-none" key={index}>
             <a href={`/chapters/${value.name}`} className="w-full h-full flex">
                 <h5 className="text-2xl m-auto text-white">{value.name}</h5>
             </a>
