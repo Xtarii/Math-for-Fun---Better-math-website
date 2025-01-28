1602e15f5c535679f9eab5b85d3449c86ea24cd5
Xtarii <carlalvinelias@gmail.com>
2025-01-28 13:14:23 +0100

    message reapers

--- components/ui/submit/panel.tsx
+++ components/ui/submit/panel.tsx
@@ -55,7 +55,10 @@ export default function SubmitPanel(props: {status: number, onClick?: (event: Mo
                     marginInlineEnd: "0.5rem",
                     marginBottom: "0.5rem"
 
-                }} variant="contained" color="info" onClick={props.onClick}>
+                }} variant="contained" color="info" onClick={(e) => {
+                    if(props.onClick) props.onClick(e);
+                    setShowMessage(true); // Shows Message
+                }}>
                     <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 11.917 9.724 16.5 19 7.5"/>
                     </svg>
