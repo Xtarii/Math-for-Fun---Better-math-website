2287ef9c5172a8d7eaf3c71e2a02070f637bbc4d
Xtarii <carlalvinelias@gmail.com>
2025-01-28 13:14:05 +0100

    faq style

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -3,10 +3,10 @@ import MathParagraph from "@/components/mathParagraph/mathParagraph";
 import { ReactElement } from "react";
 
 export default function FAQ() : ReactElement {
-    return(<div className="w-screen">
-        <h1>Hjälp med Editeraren</h1>
+    return(<div className="w-full h-fit">
+        <h1 className="text-center mx-auto mt-4">Hjälp med Editeraren</h1>
 
-        <div className="w-3/6 m-8">
+        <div className="w-2/4 h-fit m-auto mt-2 border-slate-500 border-b-2">
             <p>
                 Editeraren använder Latex för att hitta matte
                 bitar i det som användaren skriver.
@@ -20,10 +20,9 @@ export default function FAQ() : ReactElement {
                 Lika väl ska en Latex sträng sluta med $ teknet.
             </p>
 
-            <MathEditor className="w-full h-[32rem] m-auto" />
+            <MathEditor className="w-[42rem] h-[32rem] mb-6" />
         </div>
-
-        <div className="w-3/6 m-8">
+        <div className="w-2/4 h-fit m-auto mt-24">
             <p>
                 För att skriva en Latex sträng
             </p>
@@ -35,7 +34,7 @@ export default function FAQ() : ReactElement {
                 till en matte sträng.
             </p>
 
-            <MathEditor className="w-full h-[32rem] m-auto" />
+            <MathEditor className="w-[42rem] h-[32rem] mb-6" />
         </div>
     </div>);
 }
